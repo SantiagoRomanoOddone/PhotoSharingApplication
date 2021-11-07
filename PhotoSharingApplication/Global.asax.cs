c02a42ea87c5a792241ca3b601b2121a369a0a46
SantiagoRomanoOddone <santiagogabrielro@hotmail.com>
2021-11-07 20:27:56 -0300

    Agregar archivos de proyecto.

---
+++ PhotoSharingApplication/Global.asax.cs
@@ -0,0 +1,22 @@
+using PhotoSharingApplication.Models;//a
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;//a
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace PhotoSharingApplication
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            // COMO ACTIVAR LA CLASE INICIALIZADORA
+            Database.SetInitializer<PhotoSharingContext>(new PhotoSharingInitializer());
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
---
+++ PhotoSharingApplication/Models/PhotoSharingContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity; //a
+namespace PhotoSharingApplication.Models
+{
+    //Argreg DbContext
+    public class PhotoSharingContext : DbContext
+    {
+        //Lo agregue yo
+        public PhotoSharingContext() : base("KeyDB") { }
+
+        public DbSet<Photo> Photos { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+
+    }
+}
